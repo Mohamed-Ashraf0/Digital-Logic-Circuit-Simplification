828ef33578d8d75b7338e89e1220035345812b5b
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-12-14 01:02:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,120 +1,167 @@
 #include <iostream>
-
 using namespace std;
 
-#define ORIGINAL_EXPRESSION(A, B, C) (((A && B) || ((B || C) && (B && C)) || ((B && C) && (!(B && C)))))
-#define SIMPLIFIED_EXPRESSION(A, B, C) ((A && B) || (B && C))
+#define ORIGINAL_EXPRESSION(A, B, C) (((!( A || !(B))) &&  (A && B)) && C )
+#define SIMPLIFIED_EXPRESSION(A, B, C) !A && A && B && C
+//by changeing 1 gate of unmodified circuit
+#define MODIFIED_ORIGINAL_EXPRESSION(A, B, C) (((!( A || !(B))) ||  (A && B)) && C )
+#define MODIFIED_SIMPLIFIED_EXPRESSION(A, B, C) B && C
+#define ORIGINAL_CIRCUIT 1
+#define MODIFIED_CIRCUIT 0
+
+// all inputs combinations
+bool truthTable[8][3] = {
+    {1, 1, 1},
+    {1, 1, 0},
+    {1, 0, 1},
+    {1, 0, 0},
+    {0, 1, 1},
+    {0, 1, 0},
+    {0, 0, 1},
+    {0, 0, 0}};
 
 void printTruthTable(bool truthTable[8][3], bool circuitoutput[8], string circuitName)
 {
-  cout << "Truth Table for " << circuitName << ":" << "\n";
-  for (int i = 0; i < 8; ++i)
-  {
-    cout << truthTable[i][0] << " " << truthTable[i][1] << " " << truthTable[i][2] << " : " << circuitoutput[i] << "\n";
-  }
+    cout << "Truth Table for " << circuitName << ":" << "\n";
+    for (int i = 0; i < 8; ++i)
+    {
+        cout << truthTable[i][0] << " " << truthTable[i][1] << " " << truthTable[i][2] << " : " << circuitoutput[i] << "\n";
+    }
 }
 
-void checkOutputsEqual(bool originalTruthTable[8], bool simplifiedTruthTable[8])
+bool check_equivalance(bool status)
 {
-  for (int i = 0; i < 8; ++i)
-  {
-    if (originalTruthTable[i] != simplifiedTruthTable[i])
+    cout << "checking equivalance:-\n";
+    bool A, B, C;                      // circuit inputs
+    bool Original_Circuit_output[8];   // outputs of Original Circuit
+    bool Simplified_Circuit_output[8]; // outputs of Simplified Circuit
+    bool not_equivalent = false; // outputs of Simplified Circuit
+
+    // evaluating output of original and simplified circuit
+    for (int i = 0; i < 8; ++i)
     {
-      cout << "Expressions are not equivalent\n";
-      return;
+        A = truthTable[i][0];
+        B = truthTable[i][1];
+        C = truthTable[i][2];
+
+        if (status) // modified or unmodified
+        {
+            Original_Circuit_output[i] = ORIGINAL_EXPRESSION(A, B, C);
+            Simplified_Circuit_output[i] = SIMPLIFIED_EXPRESSION(A, B, C);
+        }
+        else
+        {
+            Original_Circuit_output[i] = MODIFIED_ORIGINAL_EXPRESSION(A, B, C);
+            Simplified_Circuit_output[i] = MODIFIED_SIMPLIFIED_EXPRESSION(A, B, C);
+        }
+
+        if (Original_Circuit_output[i] != Simplified_Circuit_output[i])
+        {
+            not_equivalent = true;
+        }
     }
-  }
-  cout << "Expressions are equivalent\n";
+
+    printTruthTable(truthTable, Original_Circuit_output, "Original Circuit");
+    printTruthTable(truthTable, Simplified_Circuit_output, "Simplified Circuit");
+
+    if (not_equivalent)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+bool check_Satisfiability(bool status)
+{
+    cout << "checking Satisfiability:-\n";
+    bool A, B, C;                      // circuit inputs
+    bool Original_Circuit_output[8];   // outputs of Original Circuit
+    bool Simplified_Circuit_output[8]; // outputs of Simplified Circuit
+    bool first_time = true;
+
+    // evaluating output of original and simplified circuit
+    for (int i = 0; i < 8; ++i)
+    {
+        A = truthTable[i][0];
+        B = truthTable[i][1];
+        C = truthTable[i][2];
+
+        if (status)
+        {
+            Original_Circuit_output[i] = ORIGINAL_EXPRESSION(A, B, C);
+            Simplified_Circuit_output[i] = SIMPLIFIED_EXPRESSION(A, B, C);
+        }
+        else
+        {
+            Original_Circuit_output[i] = MODIFIED_ORIGINAL_EXPRESSION(A, B, C);
+            Simplified_Circuit_output[i] = MODIFIED_SIMPLIFIED_EXPRESSION(A, B, C);
+        }
+
+        if (Original_Circuit_output[i] == true && Simplified_Circuit_output[i] == true)
+        {
+            if (first_time)
+            {
+                cout << "Satisfiable\nSatisfiable inputs:-\n";
+                first_time = false;
+            }
+            cout << A << " " << B << " " << C << "\n";
+        }
+    }
+
+    if (first_time)
+    {
+        cout << "not Satisfiable\n";
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 int main()
 {
-  bool A, B, C;                      // circuit inputs
-  bool Original_Circuit_output[8];   // outputs of Original Circuit
-  bool Simplified_Circuit_output[8]; // outputs of Simplified Circuit
-  // all inputs combinations
-  bool truthTable[8][3] = {
-      {1, 1, 1},
-      {1, 1, 0},
-      {1, 0, 1},
-      {1, 0, 0},
-      {0, 1, 1},
-      {0, 1, 0},
-      {0, 0, 1},
-      {0, 0, 0}};
-  // satsifiable inputs for both original and simplified
-  bool Satisfiable_inputs[8][3];
-  bool satisfiable = 0;
-  int j = 0; // counter for number of inputs statsify both experssion
-  int x = 0; // to choose which modified simplified expression to choose
-  bool y;
-  bool z;
-  // evaluating output of original and simplified circuit
-  for (int i = 0; i < 8; ++i)
-  {
-    A = truthTable[i][0];
-    B = truthTable[i][1];
-    C = truthTable[i][2];
-    Original_Circuit_output[i] = ORIGINAL_EXPRESSION(A, B, C);
-    Simplified_Circuit_output[i] = SIMPLIFIED_EXPRESSION(A, B, C);
-  }
-
-  printTruthTable(truthTable, Original_Circuit_output, "Original Circuit");
-  printTruthTable(truthTable, Simplified_Circuit_output, "Simplified Circuit");
-
-  checkOutputsEqual(Original_Circuit_output, Simplified_Circuit_output);
-
-  // satisfiabillity check part
-  while (!satisfiable)
-  {
-    for (int i = 0; i < 8; ++i)
+    // equivalance check part
+    if (check_equivalance(ORIGINAL_CIRCUIT))
     {
-      A = truthTable[i][0];
-      B = truthTable[i][1];
-      C = truthTable[i][2];
-      y = ORIGINAL_EXPRESSION(A, B, C);
-      switch (x)
-      {
-      case 0:
-        z = (A && B) || (B && C);
-        break;
-      case 1:
-        z = (A || B) || (B && C); // fist modification
-        break;
-      case 2:
-        z = (A && B) || (B || C); // second modification
-        break;
-      case 3:
-        z = (A && B) && (B && C); // third modification
-        break;
-      }
-      if (y == 1 && z == 1)
-      {
-        satisfiable = 1;
-        Satisfiable_inputs[j][0] = A;
-        Satisfiable_inputs[j][1] = B;
-        Satisfiable_inputs[j][2] = C;
-        j++;
-      }
-    }
-    if (satisfiable)
-    {
-      cout << "Satisfiable_inputs\n";
-      for (int i = 0; i < j; i++)
-      {
-        cout << Satisfiable_inputs[i][0] << " " << Satisfiable_inputs[i][1] << " " << Satisfiable_inputs[i][2] << "\n";
-      }
+        cout << "Expressions are equivalent.\n";
     }
     else
     {
-      x = x++; // modify the simplified expression
-      cout << "modifying the simplified expression";
-      if (x == 4)
-      {
-        cout << "unable to modify circuit to be Satisfiable";
-        break;
-      }
+        cout << "Expressions are not equivalent.\n";
     }
-  }
-  return 0;
+    cout << "\n";
+
+    // satisfiabillity check part
+    if (check_Satisfiability(ORIGINAL_CIRCUIT))
+    {
+        return 0; // finish program
+    }
+    else
+    {
+        cout << "\nTrying modified circuit...\n\n";
+
+        if (check_Satisfiability(MODIFIED_CIRCUIT))
+        {
+            cout << "\n";
+
+            if (check_equivalance(MODIFIED_CIRCUIT))
+            {
+                cout << "Expressions are equivalent.\n";
+            }
+            else
+            {
+                cout << "Expressions are not equivalent.\n";
+            }
+        }
+        else
+        {
+            cout << "Changed gate but still not Satisfiable\n";
+        }
+    }
+
+    return 0;
 }
